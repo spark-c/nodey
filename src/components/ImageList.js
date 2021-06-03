855dc08d26b36de8e51e531bd1f732ba3c8597a4
Collin Sparks <cklsparks@gmail.com>
2021-06-03 18:15:18 -0500

    img-search v2 complete

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import ImageCard from './ImageCard';
+import './ImageList.css';
 
 const ImageList = (props) => {
     const images = props.images.map((image) => {
-        return <img key={image.id} alt={image.description} src={image.urls.small} />
+        return <ImageCard key={image.id} image={image} />
         // the "key" prop is a unique idenitifier that helps React perform
         // as well as possible when updating lists of components
     });
 
     return (
-        <div>
+        <div className="image-list">
             {images}
         </div>
     );
