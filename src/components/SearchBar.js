5bfda279aa03814996e124bb2e225644244f1841
Collin Sparks <cklsparks@gmail.com>
2021-06-04 18:47:39 -0500

    updated gitignore to ignore YT api json

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,7 +14,13 @@ class SearchBar extends React.Component {
             <div className='ui segment'>
                 <form onSubmit={this.props.onSubmit} className='ui form'>
                     <label>Video Search</label>
-                    <input onChange={this.onInputChange} value={this.state.query} className='field' type='text' placeholder='Enter your query...'></input>
+                    <input 
+                    onChange={this.onInputChange} 
+                    value={this.state.query} 
+                    className='field' 
+                    type='text' 
+                    placeholder='Enter your query...'
+                    />
                 </form>
             </div>
         );
