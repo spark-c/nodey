f18c28b727986cfd9a8928d1397852cf45840581
Collin Sparks <cklsparks@gmail.com>
2021-06-04 18:29:42 -0500

    layout mostly complete, need api

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,8 +5,16 @@ class VideoItem extends React.Component {
 
     render() {
         return (
-            <div className="video-item ui card" onClick={() => this.props.onVideoItemClick(this)}>
-                {this.props.video.title}
+            <div className="video-item" onClick={() => this.props.onVideoItemClick(this)}>
+                <img className="thumbnail" src="" alt="thumbnail" ></img>
+                <div className="text-data">
+                    <div className="title">Title</div> {/* this.props.video.title */}
+                    <div className="channel">By Channel Name</div> {/* this.props.video.author */}
+                    <div className="metadata">
+                        <div className='views'>9999 Views</div>
+                        <div className="timestamp">1 Month Ago</div>
+                    </div>
+                </div>
             </div>
         );
     }
