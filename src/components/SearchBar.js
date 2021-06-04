ead4f1f7066d3f83763905953f12a474683ea0fa
Collin Sparks <cklsparks@gmail.com>
2021-06-04 18:34:43 -0500

    added label to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,6 +13,7 @@ class SearchBar extends React.Component {
         return (
             <div className='ui segment'>
                 <form onSubmit={this.props.onSubmit} className='ui form'>
+                    <label>Video Search</label>
                     <input onChange={this.onInputChange} value={this.state.query} className='field' type='text' placeholder='Enter your query...'></input>
                 </form>
             </div>
