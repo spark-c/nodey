d18ef58680b0149a92558682b0e02894de07af56
Collin Sparks <cklsparks@gmail.com>
2021-06-05 18:07:30 -0500

    wiki results populate and timeout works

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import "./Search.css"
 import axios from 'axios';
 
 
 const Search = () => {
     const [query, setQuery] = useState("");
+    const [results, setResults] = useState([])
 
     useEffect(() => {
         const searchWiki = async () => {
-            await axios.get("https://en.wikipedia.org/w/api.php", {
+            const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
                 params: {
                     action: "query",
                     list: "search",
@@ -16,15 +18,47 @@ const Search = () => {
                     srsearch: query
                 }
             });
+
+            setResults(data.query.search);
         };
-        searchWiki();
+
+        const timerId = setTimeout(() => {
+            if (query) {
+                searchWiki();
+            }
+        }, 500);
+        
+
+        return () => {
+            clearTimeout(timerId);
+        };
     }, [query]);
 
+    const clean = (str) => {
+        return str.replaceAll("<span class=\"searchmatch\">", "").replaceAll("</span>", "");
+    };
+
+    const resultsList = results.map((result) => {
+        return (
+            <a href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+                <div className="result-card" key={result.pageid}>
+                    <div className="result-title">{result.title}</div>
+                    <div className="result-description">{clean(result.snippet)}</div>
+                </div>
+            </a>
+        );
+    });
+
     return (
-        <div className="ui form">
-            <div className="field">
-                <label>Wikipedia Search</label>
-                <input className="input" onChange={(e) => setQuery(e.target.value)} value={query} type="text" placeholder="Search..." />
+        <div className="search-component">
+            <div className="ui form">
+                <div className="field">
+                    <label>Wikipedia Search</label>
+                    <input className="input" onChange={(e) => setQuery(e.target.value)} value={query} type="text" placeholder="Search..." />
+                </div>
+            </div>
+            <div>
+                {resultsList}
             </div>
         </div>
     );
